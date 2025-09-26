24b5afb68a32d9ee439ea6a0d54255c5f528d803
LAURA JIMENA HERREÑO RUBIANO <lherreno@ucundinamarca.edu.co>
2025-09-26 18:23:21 -0500

    PB011-HU002: Avance en html y css de la vista de consultar usuario. PB0011-HU004: Avance en html y css para mostrar mensaje de confirmación para inactivar usuario.

---
+++ Controllers/AdministradorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduConnect_Front.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        // GET: TutoradoController
+        public ActionResult ConsultarUsuarios()
+        {
+            return View();
+        }
+
+    }
+}
